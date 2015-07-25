709bb45a2a7a6e8b256f3692a73ddf82c56adc57
KSidgam <kali.sidagam@gmail.com>
2015-07-25 19:45:17 +0530

    Adding files

---
+++ server.r
@@ -0,0 +1,11 @@
+x<<-0
+y<<-0
+shinyServer( 
+  function(input, output) {
+   
+  
+    output$text1 <- renderText({input$text1})
+    output$text2 <- renderText({input$text2})
+    output$text3 <- renderText({as.numeric(input$text1)-as.numeric(input$text2)}) 
+    output$text4 <- renderText({((as.numeric(input$text1)-as.numeric(input$text2))/as.numeric(input$text1))*100})
+  } )
